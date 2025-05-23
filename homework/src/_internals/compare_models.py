5ef2a83233ba86a7fd4d39e1d3a57a74a0cdf66e
CamiloPerez <brcperezro@unal.edu.co>
2025-05-22 22:19:14 -0500

    Taller 06

---
+++ homework/src/_internals/compare_models.py
@@ -0,0 +1,6 @@
+def compare_models(current_model, best_model, x_test, y_test):
+    if best_model is None or current_model.score(x_test, y_test) > best_model.score(
+        x_test, y_test
+    ):
+        return current_model
+    return best_model
---
+++ homework/src/_internals/print_metrics.py
@@ -0,0 +1,5 @@
+def print_metrics(title, mse, mae, r2):
+    print(f"\n{title}:")
+    print(f"  MSE: {mse}")
+    print(f"  MAE: {mae}")
+    print(f"  R2: {r2}")
